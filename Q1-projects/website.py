80cd439bcd11e1191b5517805298c060ba6b5a8a
Jahan khan <aifreelancermj@gmail.com>
2025-04-12 15:15:23 +0500

    website.py

--- Q1-projects/website.py
+++ Q1-projects/website.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt       "igot n errer in streamlit"
 
 st.title ("Simple Data Dashboard")
 uploaded_file = st.file_uploader("chose a csv file",type = "csv")
